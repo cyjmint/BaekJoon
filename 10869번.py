fdee4f29b192b20ab3c5dc450a306355db7b507f
CYJ <qweiop1846@gmail.com>
2023-05-16 01:44:31 +0900

    10869번

---
+++ 10869번.py
@@ -0,0 +1,14 @@
+import math
+
+A, B = map(int, input().split())
+if A in list(range(1, 10001)) and B in list(range(1, 10001)):
+    print(A+B)
+    print(A-B)
+    print(A*B)
+    print(math.trunc(A/B))
+    print(A%B)
+else:
+    pass
+    
+    
+
