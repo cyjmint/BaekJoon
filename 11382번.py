17e10256e55bc2eeea6a744315ca559b88c2c9b7
cyjmint <qweiop1846@gmail.com>
2023-05-22 15:32:57 +0900

    파일 위치 변경

--- 11382번.py
+++
@@ -1,5 +0,0 @@
-A, B, C = list(map(int,input().split()))
-r = range(1,10**12+1)
-if A in r and B in r and C in r:
-    print(A + B + C)
-    
--- 18108번.py
+++
@@ -1,6 +0,0 @@
-#태국의 불기 연도를 서기 연도로 변환
-y = int(input())
-if y in list(range(1000,3001)):
-    print(y-543)
-else:
-    pass
