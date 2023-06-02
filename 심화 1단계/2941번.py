901c61e583d74f91b87aa3630e5d41f1ce03f5fd
cyjmint <qweiop1846@gmail.com>
2023-06-02 18:19:09 +0900

    틀린 코드 문제를 너무 어렵게 생각하고 작성함

--- 심화 1단계/2941번.py
+++ 심화 1단계/2941번.py
@@ -1,8 +1,5 @@
 #어떻게 같은 알파벳을 중복 처리하지 않게 하지
 #dz= 와 z=를 어떻게 구분하지 : 만약 dz=와 z=가 같이 있다면, z=의 개수에서 dz=개수를 빼야함
-from string import ascii_lowercase
-alp = list(ascii_lowercase)
-
 croatia = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z=']
 S = input()
 l = list(S)
@@ -24,8 +21,6 @@ for i in range(len(cnt)):
     else:
         n += cnt[i]
 
-l = [value for value in l if value in alp]
-
 if all(num < 2 for num in cnt):
     n += len(l)
 else:
@@ -33,4 +28,8 @@ else:
 
 print(n)
 
-#l이 완성된 이후에 l의 길이를 구해야함
+#반례1 
+#jnjnjnjn (정답 : 5)
+
+#반례2
+#c=c=c=jnjn (정답 : 6)
