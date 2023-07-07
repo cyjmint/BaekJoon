8d6468fec9e91bc9d9babe94c1d4d4d682790b8a
cyjmint <qweiop1846@gmail.com>
2023-07-07 18:20:05 +0900

    15829번 문제 *재귀함수를 사용할 때는 연산을 역순으로 생각하기*

--- 기타 문제/15829번.py
+++ 기타 문제/15829번.py
@@ -16,3 +16,14 @@ def H(n):
         return H(n-1) + h(n-1)
         
 print(H(L)%M)
+
+#l = 5
+#H = i : 0~4까지의 ai*r^i의 합을 M으로 나눈 나머지
+
+#H(5) = H(4) + hash(4)
+#H(4) = H(3) + hash(3)
+#H(3) = H(2) + hash(2)
+#H(2) = H(1) + hash(1)
+#H(1) = hash(0)
+
+#hash(n) = an*r^n
