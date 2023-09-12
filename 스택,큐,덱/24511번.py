07cf6c84db73b14c25f0ebd77b2ffe2361e72ab4
cyjmint <qweiop1846@gmail.com>
2023-09-12 20:04:02 +0900

    24511번 문제 *이 문제에서 스택은 후입선출의 특성을 지니고 있기에 필요가 없음.* *문제에서 자료구조 내의 마지막 원소부터 리턴한다고 했으므로, 스택 자료구조를 제외한 큐 자료구조의 원소를 역순으로 리턴.* *이때 리턴한 값의 개수가 M보다 작으면 부족한 개수만큼 수열 C의 원소를 앞에서부터 차례대로 리턴*

---
+++ 스택,큐,덱/24511번.py
@@ -0,0 +1,28 @@
+from collections import deque
+#import sys
+#input = sys.stdin.readline
+
+n = int(input())
+a = list(map(int,input().split()))
+b = list(map(int,input().split()))
+m = int(input())
+c = deque(map(int,input().split()))
+
+b2 = deque()
+for i in range(n):
+    if a[i] == 0:
+        b2.append(b[i])
+    
+x = []
+if len(b2) >= m:
+    for _ in range(m):
+        x.append(b2.pop())
+else:
+    for _ in range(len(b2)):
+        x.append(b2.pop())
+
+if len(x) != m:
+    for _ in range(m-len(x)):
+        x.append(c.popleft())
+
+print(*x)
