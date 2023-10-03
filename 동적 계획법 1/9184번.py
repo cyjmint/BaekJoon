d56f4dc4fd2f27227e47e7eda893762a74a896fd
cyjmint <qweiop1846@gmail.com>
2023-10-03 14:09:54 +0900

    9184번 문제 *2가지 풀이*

--- 동적 계획법 1/9184번.py
+++ 동적 계획법 1/9184번.py
@@ -1,4 +1,5 @@
-l = []
+#Memoization 방식 (Top-down)
+l = [] 
 for _ in range(21):
     l.append([[' ']*21 for _ in range(21)])
 
@@ -22,6 +23,47 @@ def w(a,b,c):
         return l[a][b][c]
         
         
+while True:
+    a,b,c = map(int,input().split())
+    if a == -1 and b == -1 and c == -1:
+        break
+    else:
+        print(f'w({a}, {b}, {c}) = {w(a,b,c)}')
+
+#%%
+#Tabulation 방식 (Bottom-up)
+l = []
+for _ in range(21):
+    l.append([[' ']*21 for _ in range(21)])
+
+def w(a,b,c):
+    if a <= 0 or b <= 0 or c <= 0:
+        a = 0; b = 0; c = 0
+    if a > 20 or b > 20 or c > 20:
+        a = 20; b = 20; c = 20
+        
+    for i in range(a+1):
+        for j in range(b+1):
+            for z in range(c+1):
+                if i == 0 or j == 0 or z == 0:
+                    if l[i][j][z] == ' ':
+                        l[i][j][z] = 1
+                    else:
+                        pass
+                else:
+                    if i < j and j < z:
+                        if l[i][j][z] == ' ':
+                            l[i][j][z] = l[i][j][z-1] + l[i][j-1][z-1] - l[i][j-1][z]
+                        else:
+                            pass
+                    else:
+                        if l[i][j][z] == ' ':
+                            l[i][j][z] = l[i-1][j][z] + l[i-1][j-1][z] + l[i-1][j][z-1] - l[i-1][j-1][z-1]
+                        else:
+                            pass
+                        
+    return l[a][b][c]
+
 while True:
     a,b,c = map(int,input().split())
     if a == -1 and b == -1 and c == -1:
