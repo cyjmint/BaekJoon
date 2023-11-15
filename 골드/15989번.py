2af651b4f691d5494186b2230b0c3f8b56052523
cyjmint <qweiop1846@gmail.com>
2023-11-15 19:52:45 +0900

    15989번 문제

---
+++ 골드/15989번.py
@@ -0,0 +1,16 @@
+f = [[0]*4 for _ in range(10001)]
+f[1][1] = 1
+f[2][1] = 1
+f[2][2] = 1
+f[3][1] = 1
+f[3][2] = 1
+f[3][3] = 1
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    for i in range(4,n+1):
+        f[i][1] = f[i-1][1]
+        f[i][2] = f[i-2][1] + f[i-2][2]
+        f[i][3] = f[i-3][1] + f[i-3][2] + f[i-3][3]
+    print(sum(f[n]))
