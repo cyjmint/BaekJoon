1db3e1490a5fbdb91077ac066e8443bcf65d9f04
cyjmint <qweiop1846@gmail.com>
2023-11-16 16:37:33 +0900

    9663번 문제

---
+++ 백트래킹/9663번.py
@@ -0,0 +1,25 @@
+n = int(input())
+col = [0]*n
+ans = 0
+
+def promising(i):
+    for k in range(i):
+        if col[i] == col[k] or abs(i-k) == abs(col[i]-col[k]):
+            return False
+
+    return True
+
+def nqueen(i):
+    global ans
+    if i == n:
+        ans += 1
+        return
+        
+    else:
+        for j in range(n):
+            col[i] = j
+            if promising(i):
+                nqueen(i+1)
+
+nqueen(0)
+print(ans)
