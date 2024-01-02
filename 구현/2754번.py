89ad49522547eeb80ee76809d0551001cdf70364
cyjmint <qweiop1846@gmail.com>
2024-01-02 21:34:27 +0900

    2754번 문제

---
+++ 구현/2754번.py
@@ -0,0 +1,27 @@
+s = input()
+if s == 'A+':
+    print(4.3)
+if s == 'A0':
+    print(4.0)
+if s == 'A-':
+    print(3.7)
+if s == 'B+':
+    print(3.3)
+if s == 'B0':
+    print(3.0)
+if s == 'B-':
+    print(2.7)
+if s == 'C+':
+    print(2.3)
+if s == 'C0':
+    print(2.0)
+if s == 'C-':
+    print(1.7)
+if s == 'D+':
+    print(1.3)
+if s == 'D0':
+    print(1.0)
+if s == 'D-':
+    print(0.7)
+if s == 'F':
+    print(0.0)
